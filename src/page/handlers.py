807076341af472951d1290eb92ba4a340f0d4282
Mr Dao <daogiatuank54@gmail.com>
2022-08-31 15:52:42 +0700

    handler for the product page

--- src/page/handlers.py
+++ src/page/handlers.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 import cloudscraper
 from bs4 import BeautifulSoup, Tag
@@ -56,3 +56,46 @@ class ListingPageHandler:
                 area = None
             result.append((title, price, price_per_m2, area, date, url))
         return result
+
+
+class ProductPageHandler:
+    def __init__(self):
+        self.page = None
+
+    def set_page(self, page):
+        self.page = page
+        return self
+
+    def get_items(self) -> List:
+        result = self.__get_items_data_pretty()
+        if result == [] or result is None:
+            return self.get_items()
+        return result
+
+    def __get_items_data_pretty(self) -> Dict:
+        scraper: CloudScraper = cloudscraper.create_scraper()
+        result: List[Tuple] = []
+        scraper: CloudScraper = scraper
+        data: Response = scraper.get(self.page)
+        data.encoding = 'utf-8'
+        print(data.status_code)
+        soup: BeautifulSoup = BeautifulSoup(data.text, 'html.parser')
+        try:
+            product_details = soup.select(
+                "#product-detail-web > div.re__section.re__pr-specs.re__pr-specs-v1.js__section > div > div")[0]
+        except IndexError:
+            return []
+        item_titles = product_details.select('.re__pr-specs-content-item-title')
+        item_values = product_details.select('.re__pr-specs-content-item-value')
+        result = {}
+        for title, value in zip(item_titles, item_values):
+            result[title.text] = value.text
+        return result
+
+
+if __name__ == '__main__':
+    prodhandler = ProductPageHandler()
+    page = 'https://batdongsan.com.vn/ban-condotel-xa-bao-ninh-prj-dolce-penisola-quang-binh/ban-can-codoltel-view-bien-gia-1-2-ty-1-can-thanh-toan-tu-3-5-pr34662414'
+    prodhandler.set_page(page)
+    product_details = prodhandler.get_items()
+    print(product_details)
